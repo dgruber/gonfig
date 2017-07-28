34d0dff6ca7ce159310fdf8f2c901459077edee5
dgruber <mail@danielgruber.com>
2017-07-28 13:20:04 +0200

    fly

--- ci/run.sh
+++ ci/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # example for setting up pipeline in concourse
+# fly -t ci login  ci.route.today
 
 fly -t ci set-pipeline --config pipeline.yml --load-vars-from params.yml -p gonfig
 fly -t ci up -p gonfig
